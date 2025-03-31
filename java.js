c5f08c2afa0479d85254d5803cf87432923ae48b
b0559217 <b0559217@amarillocollege.com>
2025-03-30 23:19:41 -0500

    Add files via upload
    if and else

---
+++ java.js
@@ -0,0 +1,17 @@
+function displayGreeting() {
+    const currentHour = new Date().getHours();
+    let greeting = "";
+
+    if (currentHour < 12) {
+        greeting = "Good Morning!";
+    } else if (currentHour < 18) {
+        greeting = "Good Afternoon!";
+    } else {
+        greeting = "Good Evening!";
+    }
+
+    document.getElementById("greeting").textContent = greeting;
+}
+
+// Call the function
+displayGreeting();
